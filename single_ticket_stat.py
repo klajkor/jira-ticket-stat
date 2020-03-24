ca523762510aade3639055a997490b4fd13e90c7
Robert Klajko <klajkor@gmail.com>
2020-03-24 22:18:41 +0100

    CSV export 1st version

--- single_ticket_stat.py
+++ single_ticket_stat.py
@@ -1,9 +1,11 @@
 # library modules
 import sys, os, traceback, argparse
 from datetime import datetime
+from datetime import timedelta
 import time
 import re
 import ast
+import csv
 
 from jira import JIRA
 import pandas as pd
@@ -23,22 +25,38 @@ options = {
 
 jira = JIRA(options, basic_auth=(jira_user,jira_apikey) )
 
-ticket = 'CCP-1545'
+ticket = 'CCP-1258'
 issue = jira.issue(ticket, expand='changelog')
+key = issue.key
+type_name = issue.fields.issuetype.name
 summary = issue.fields.summary
-print('ticket: ', ticket, issue.fields.summary)
+status = issue.fields.status.name
+version_name = issue.fields.fixVersions[0].name
+story_point = issue.fields.customfield_10105
+last_sprint = str(re.findall(r"name=[^,]*", issue.fields.customfield_10103[0]))
+last_sprint = last_sprint.split("=")[1]
+last_sprint = last_sprint.split("'")[0]
+single_ticket = [key, type_name, summary, status, version_name, story_point, last_sprint]
+#print(key, ';', type_name, ';', summary, ';', status, ';', version_name, ';', story_point, ';', last_sprint)
+print(single_ticket)
 
-print(f"Changes from issue: {issue.key} {issue.fields.summary}")
-print(f"Number of Changelog entries found: {issue.changelog.total}") # number of changelog entries (careful, each entry can have multiple field changes)
+history_header = ['Issue key', 'Author', 'Timestamp', 'Item type', 'From', 'To']
+raw_history = []
 
 for history in issue.changelog.histories:
-    print(f"Author: {history.author}") # person who did the change
-    print(f"Timestamp: {history.created}") # when did the change happen?
-    #print("\nListing all items that changed:")
-
     for item in history.items:
-        if item.field == 'status':
-            print(f"Field name: {item.field}") # field to which the change happened
-            print(f"Changed from: {item.fromString}") # old value, item.from might be better in some cases depending on your needs.
-            print(f"Changed to: {item.toString}") # new value, item.to might be better in some cases depending on your needs.
-            print()
+        if item.field == 'status' or item.field == 'assignee':
+            created_ts = datetime.strptime(str(history.created), '%Y-%m-%dT%H:%M:%S.%f%z')
+            created_ts = datetime.strftime(created_ts, '%Y-%m-%d %H:%M:%S')
+            history_item = [key, history.author.displayName, created_ts, item.field, item.fromString, item.toString]
+            raw_history.append(history_item)
+            #print(key, ';', history.author.displayName, ';', created_ts, ';', item.field, ';', item.fromString, ';', item.toString)
+
+raw_history = sorted(raw_history, key=lambda l:l[2])
+
+csv_file_name = key + '_history.csv'
+with open(csv_file_name, mode='w', newline='', encoding='utf-8') as csv_file:
+    csv_file = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    csv_file.writerow(history_header)
+    for item in raw_history:
+        csv_file.writerow(item)

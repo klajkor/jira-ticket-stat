a13b9cfff42bcd3b4aac241a3ad7801174251854
Robert Klajko <klajkor@gmail.com>
2020-04-06 21:38:42 +0200

    Tickets history in one file.

--- single_ticket_stat.py
+++ single_ticket_stat.py
@@ -30,9 +30,10 @@ ticket_list_file = 'ticket_list.csv'
 ticket_history_file = 'ticket_history.csv'
 ticket_header = ['Issue key', 'Type', 'Summary', 'Status', 'Release', 'Story Point', 'Last Sprint', 'Created', 'Updated', 'Epic Key', 'Epic Title', 'Labels']
 ticket_raw = []
-history_header = ['Issue key', 'Author', 'Timestamp', 'Item type', 'From', 'To']
+history_header = ['Issue key', 'Issue ID', 'Author', 'Timestamp', 'Item type', 'From', 'To']
 history_raw = []
 all_epics = {}
+issues_in_proj = {}
 
 def get_all_epics(query_string):
     tmp_dict = {}
@@ -43,8 +44,8 @@ def get_all_epics(query_string):
 
 
 def run_JQL_query(query_string):
-    issues_in_proj = jira.search_issues(query_string, maxResults=1000)
-    for issue in issues_in_proj:
+    issues_in_query = jira.search_issues(query_string, maxResults=1000)
+    for issue in issues_in_query:
         type_name = issue.fields.issuetype.name
         summary = issue.fields.summary
         status = issue.fields.status.name
@@ -74,46 +75,39 @@ def run_JQL_query(query_string):
         csv_file.writerow(ticket_header)
         for item in ticket_raw:
             csv_file.writerow(item)
-    return issues_in_proj
+    return issues_in_query
 
-jira = JIRA(options, basic_auth=(jira_user,jira_apikey) )
+def collect_all_history(issues):
+    history_raw = []
+    for history_issue in issues:
+        issue = jira.issue(history_issue.key, expand='changelog')
+        key = issue.key
+        issue_id = issue.id
 
+        for history in issue.changelog.histories:
+            for item in history.items:
+                if item.field == 'status' or item.field == 'assignee':
+                    created_ts = datetime.strptime(str(history.created), '%Y-%m-%dT%H:%M:%S.%f%z')
+                    created_ts = datetime.strftime(created_ts, '%Y-%m-%d %H:%M:%S')
+                    author_name = history.author.displayName
+                    author_name = author_name.replace("Erdélyi 💛 ", "Erdelyi")
+                    history_item = [key, issue_id, author_name, created_ts, item.field, item.fromString, item.toString]
+                    history_raw.append(history_item)
+
+    history_raw = sorted(history_raw, key=lambda x: (x[1], x[3]))
+
+    with open(ticket_history_file, mode='w', newline='', encoding='utf-8') as csv_file:
+        csv_file = csv.writer(csv_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_file.writerow(history_header)
+        for item in history_raw:
+            csv_file.writerow(item)
+
+def filter_non_printable(str):
+    return ''.join([c for c in str if ord(c) > 31 or ord(c) == 9])
+
+jira = JIRA(options, basic_auth=(jira_user,jira_apikey))
 all_epics = get_all_epics(jql_string_all_epics)
-
-ticket = 'CCP-1258'
-issue = jira.issue(ticket, expand='changelog')
-key = issue.key
-type_name = issue.fields.issuetype.name
-summary = issue.fields.summary
-status = issue.fields.status.name
-version_name = issue.fields.fixVersions[0].name
-story_point = issue.fields.customfield_10105
-last_sprint = str(re.findall(r"name=[^,]*", issue.fields.customfield_10103[0]))
-last_sprint = last_sprint.split("=")[1]
-last_sprint = last_sprint.split("'")[0]
-single_ticket = [key, type_name, summary, status, version_name, story_point, last_sprint]
-#print(single_ticket)
-
-
-
-for history in issue.changelog.histories:
-    for item in history.items:
-        if item.field == 'status' or item.field == 'assignee':
-            created_ts = datetime.strptime(str(history.created), '%Y-%m-%dT%H:%M:%S.%f%z')
-            created_ts = datetime.strftime(created_ts, '%Y-%m-%d %H:%M:%S')
-            history_item = [key, history.author.displayName, created_ts, item.field, item.fromString, item.toString]
-            history_raw.append(history_item)
-            #print(key, ';', history.author.displayName, ';', created_ts, ';', item.field, ';', item.fromString, ';', item.toString)
-
-history_raw = sorted(history_raw, key=lambda l:l[2])
-
-csv_file_name = key + '_history.csv'
-with open(csv_file_name, mode='w', newline='', encoding='utf-8') as csv_file:
-    csv_file = csv.writer(csv_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    csv_file.writerow(history_header)
-    for item in history_raw:
-        csv_file.writerow(item)
-
-run_JQL_query(jql_string_R5)
+issues_in_proj = run_JQL_query(jql_string_R5)
+collect_all_history(issues_in_proj)
 
 print("--- Execution time: %s seconds ---" % (time.time() - start_time))

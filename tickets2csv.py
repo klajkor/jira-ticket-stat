4f1a5f1d4a18402587615916f267781ac04c930e
Robert Klajko <klajkor@gmail.com>
2020-05-29 11:34:05 +0200

    several updates

--- tickets2csv.py
+++ tickets2csv.py
@@ -17,11 +17,13 @@ options = {
 jql_string_R5 = 'project = CCP AND fixVersion in ("Release 5.0") ORDER BY type DESC,  key ASC'
 jql_string_R51 = 'project = CCP AND fixVersion in ("Release 5.1") ORDER BY type DESC,  key ASC'
 jql_string_R6 = 'project = CCP AND fixVersion in ("Release 6.0") ORDER BY type DESC,  key ASC'
+jql_string_R6_R61 = 'project = CCP AND (fixVersion in ("Release 6.0") OR fixVersion in ("Release 6.1")) ORDER BY type DESC,  key ASC'
 jql_string_all_epics = 'project = CCP AND type in (Epic) ORDER BY KEY ASC'
 jql_string_sprint = 'issueKey in (CCP-640,CCP-1431,CCP-1543,CCP-1571,CCP-1633,CCP-1643,CCP-1644,CCP-1677)'
 jql_string_lms = 'project = CCP AND type in (Story) AND "Epic Link" in (CCP-1666, CCP-1651, CCP-1652) ORDER BY key ASC'
 jql_string_lms_p1 = 'project = CCP AND type in (Story) AND "Epic Link" = CCP-1666 ORDER BY key ASC'
-jql_string_sprint = 'issueKey in (CCP-640,CCP-1633,CCP-1643,CCP-1644,CCP-1649,CCP-1656,CCP-1669,CCP-1721, CCP-1431,CCP-1543,CCP-1571,CCP-1610,CCP-1665,CCP-1672,CCP-1677) '
+jql_string_sprint = 'issueKey in (CCP-1610,CCP-1677,CCP-1431,CCP-1543,CCP-1571,CCP-1584,CCP-1607,CCP-1655,CCP-1665,CCP-1672,CCP-1683,CCP-1699,CCP-1732)'
+jql_string_estimation = 'project = CCP AND type in (Story) AND labels in (refined) AND Sprint is EMPTY AND status not in (Done) AND ("Epic Link" not in (CCP-34, CCP-101) OR "Epic link" is EMPTY) ORDER BY Fixversion ASC, rank ASC'
 
 ticket_list_file = 'ticket_list.csv'
 all_epics = {}
@@ -30,7 +32,7 @@ issues_in_proj = {}
 if __name__ == '__main__':
     jira = JIRA(options, basic_auth=(jira_user, jira_apikey))
     all_epics = jira_stat_lib.get_all_epics(jira, jql_string_all_epics)
-    issues_in_proj = jira_stat_lib.export_issues_to_csv(jira, jql_string_sprint, all_epics, ticket_list_file)
+    issues_in_proj = jira_stat_lib.export_issues_to_csv(jira, jql_string_R6_R61, all_epics, ticket_list_file)
     print("#####----------------------------------#####")
     print("Tickets exported to: ", ticket_list_file)
     print("#####----------------------------------#####")
